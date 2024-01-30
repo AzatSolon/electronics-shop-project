765f4dfc23d2f7e6d71474da6ede7f689d0565f8
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-01-30 10:56:26 +0300

    add test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+product = Item("Ноутбук MSI", 100000, 10)
+
+
+def test_calculate_total_price():
+    assert product.calculate_total_price() == 500000.0
+
+
+product.pay_rate = 0.5
+product.apply_discount()
+
+
+def test_apply_discount():
+    assert product.price == 50000.0
